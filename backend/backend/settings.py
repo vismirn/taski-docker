fad94ebc1d5e56ad2dd7558a8e2270a75896ca6d
Viktor Smirnov <smirn04@yandex.ru>
2023-06-15 09:25:28 +0300

    first5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,8 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.22.84', '192.168.0.1', 'taskismi.hopto.org', 'localhost']
-
+#ALLOWED_HOSTS = ['158.160.22.84', '192.168.0.1', 'taskismi.hopto.org', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
